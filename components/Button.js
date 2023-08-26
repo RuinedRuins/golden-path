8fc32cb26a23cef25df2115a15aefeaa77e86c3b
RuinedRuins <matthew.ngjb@gmail.com>
2023-08-26 15:55:46 +0800

    feat: :rocket: - Initial Commit

---
+++ components/Button.js
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+const CustomButton = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Click Me
+    </Button>
+  );
+};
+
+export default CustomButton;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import CustomButton from "../components/Button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello Next.js with MUI!</h1>
+      <CustomButton />
+    </div>
+  );
+}
