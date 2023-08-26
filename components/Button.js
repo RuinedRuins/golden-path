f1da367e8921634a3e42dcef779f1868b859d857
RuinedRuins <matthew.ngjb@gmail.com>
2023-08-26 19:28:14 +0800

    feat: :sparkles: - Refer above

--- components/Button.js
+++ components/Button.js
@@ -1,12 +1,27 @@
 import { Button } from "@mui/material";
+import { useState } from "react";
 
-const CustomButton = ({ text, startIcon, endIcon }) => {
+const CustomButton = ({
+  text,
+  startIcon,
+  endIcon,
+  bgColor,
+  color,
+  onHoverColor,
+}) => {
   return (
     <Button
       variant="contained"
-      color="primary"
       startIcon={startIcon}
       endIcon={endIcon}
+      sx={{
+        color: color,
+        backgroundColor: bgColor,
+        fontWeight: 600,
+        "&:hover": {
+          backgroundColor: onHoverColor,
+        },
+      }}
     >
       {text}
     </Button>
